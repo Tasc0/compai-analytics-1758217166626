6c66bb4d48d3bf91d7315873e3713c9ea8d19fbf
Tasc0 <tassio.gabriel@gmail.com>
2025-09-18 14:39:29 -0300

    Add next.config.js

---
+++ next.config.js
@@ -0,0 +1,19 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+  images: {
+    domains: ['images.unsplash.com'],
+  },
+  env: {
+    OPENAI_API_KEY: process.env.OPENAI_API_KEY,
+    GOOGLE_SHEETS_PRIVATE_KEY: process.env.GOOGLE_SHEETS_PRIVATE_KEY,
+    GOOGLE_SHEETS_CLIENT_EMAIL: process.env.GOOGLE_SHEETS_CLIENT_EMAIL,
+    GOOGLE_SHEET_ID: process.env.GOOGLE_SHEET_ID,
+  },
+  experimental: {
+    serverComponentsExternalPackages: ['googleapis'],
+  },
+}
+
+module.exports = nextConfig
